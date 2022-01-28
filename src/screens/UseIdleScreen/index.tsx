a979d99874b4124aa1d756f5b8a30df54c8802b6
Irvin Ives Lau <lauirvin98@gmail.com>
2022-01-28 15:24:16 +0800

    feat: UseIdleScreen styling

--- src/screens/UseIdleScreen/index.tsx
+++ src/screens/UseIdleScreen/index.tsx
@@ -5,18 +5,19 @@ import useIdle from '../../hooks/useIdle';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
   },
 });
 
 const UseIdleScreen: React.FC = () => {
   const { isIdle, panResponder } = useIdle({ timeout: 5000 });
   return (
-    <View
-      style={{ ...styles.container, backgroundColor: isIdle ? 'red' : 'blue' }}
-      {...panResponder.panHandlers}
-    >
-      <Text>UseIdleScreen</Text>
-      <Text>{isIdle ? 'User is idle' : 'User is not Idle'}</Text>
+    <View style={{ ...styles.container, backgroundColor: isIdle ? 'red' : 'green' }} {...panResponder.panHandlers}>
+      <Text style={styles.text}>{isIdle ? 'User is idle' : 'User is Active'}</Text>
     </View>
   );
 };
